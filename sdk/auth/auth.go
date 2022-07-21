a3e297003681ca8bb82b5ccb803c474bffaef3a8
Chenxi Shao <1074654727@qq.com>
2022-07-21 10:27:29 +0800

    feat: add sdk auth

---
+++ sdk/auth/auth.go
@@ -0,0 +1,41 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var ErrInvalidValue = errors.New("invalid value")
+
+//IsRoot auth user privilege is admin or other
+func IsRoot(root string) (bool, error) {
+	switch root {
+	case "true":
+		return true, nil
+	case "false":
+		return false, nil
+	default:
+		return false, ErrInvalidValue
+	}
+}
+
+// Region auth user's region privilege
+func Region(regions string, target uint64) (bool, error) {
+	tg := fmt.Sprintf(" %d ", target)
+	if strings.Contains(regions, tg) {
+		return true, nil
+	} else {
+		return false, nil
+	}
+}
+
+// Feature auth user's feature privilege
+func Feature(features string, target uint64) (bool, error) {
+	tg := fmt.Sprintf(" %d ", target)
+	if strings.Contains(features, tg) {
+		return true, nil
+	} else {
+		return false, nil
+	}
+}
