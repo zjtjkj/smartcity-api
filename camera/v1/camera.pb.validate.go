3915601c4f8bcf79608f6c1a349dd21c891b016a
DuanWenHao <noreply@drone>
2022-08-12 08:20:47 +0000

    smartcity-api

--- camera/v1/camera.pb.validate.go
+++ camera/v1/camera.pb.validate.go
@@ -1000,6 +1000,35 @@ func (m *GetCameraReply) validate(all bool) error {
 
 	var errors []error
 
+	if all {
+		switch v := interface{}(m.GetCamera()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, GetCameraReplyValidationError{
+					field:  "Camera",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, GetCameraReplyValidationError{
+					field:  "Camera",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetCamera()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return GetCameraReplyValidationError{
+				field:  "Camera",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return GetCameraReplyMultiError(errors)
 	}
