063318361f392312a6a048d36fdc27e59d19b606
DuanWenHao <noreply@drone>
2022-09-08 03:54:44 +0000

    smartcity-api

--- settings/v1/settings.pb.validate.go
+++ settings/v1/settings.pb.validate.go
@@ -1601,7 +1601,34 @@ func (m *CameraAttr) validate(all bool) error {
 
 	// no validation rules for Id
 
-	// no validation rules for Created
+	if all {
+		switch v := interface{}(m.GetCreated()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, CameraAttrValidationError{
+					field:  "Created",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, CameraAttrValidationError{
+					field:  "Created",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetCreated()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return CameraAttrValidationError{
+				field:  "Created",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
 
 	// no validation rules for Name
 
